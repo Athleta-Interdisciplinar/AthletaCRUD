571ae63bcbd8ce2b04f622840de981c57a735aa0
fellipemeiraGerminare <fellipe.cristino@germinare.org,br>
2024-10-01 09:38:35 -0300

    CRUD analiseEve

--- AthletaServelet/src/main/java/main.java
+++ AthletaServelet/src/main/java/main.java
@@ -1,9 +1,21 @@
+import Classes.AnaliseEventos;
 import DAO.AdminDao;
+import DAO.AnaliseEventosDao;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 public class main {
     public static void main(String[] args) {
-        AdminDao adminDao = new AdminDao();
+//        AdminDao adminDao = new AdminDao();
 
-        adminDao.cadastrarAdm(23, "admin", "admin@gmail.com", "senha");
+//        adminDao.cadastrarAdm(24, "admin2", "admin2@gmail.com", "senha2");
+
+//        LocalDate dataAtual = LocalDate.now(); //Data hj
+        LocalDate dt1 =  LocalDate.of(2008,11,21);
+//        AnaliseEventos an = new AnaliseEventos(3,"Torneio de beisebbol", "torneio amador ", dt1, "Nakashima",4,6);
+//        System.out.println(an);
+        AnaliseEventosDao an2 = new AnaliseEventosDao();
+        an2.inserirEvento(3,"Torneio de beisebbol", "torneio amador ", dt1, "Nakashima","qsjnejnejqeijq",170, 34);
     }
 }
